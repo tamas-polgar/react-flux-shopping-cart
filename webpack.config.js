b22958a91e37a0637fe43d97865ad3cdf93914cd
tamas-polgar <tamas.polgar.dev@gmail.com>
2018-04-25 18:24:10 +0800

    Dealing with router issue for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: "bundle.js",
-		publicPath: '/'
+		//publicPath: '/',
+		publicPath: "/react-cart/",
 	},
 	devServer: {
 		inline: true,
